9bbd676e7e1fee675f2d6d6b60a5f3fcf786c553
Rcpdroid <amos.marbun86@gmail.com>
2026-02-13 18:56:30 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,42 @@
-const CACHE_NAME = "shift-app-cache-v1";
+const CACHE_NAME = "shift-pro-v1";
 
+const urlsToCache = [
+  "/",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/background.png"
+];
+
+// INSTALL
 self.addEventListener("install", event => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+// FETCH (offline first)
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
 });
