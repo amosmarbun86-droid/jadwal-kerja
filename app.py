b39ad64d60954e471078932a2ad4471aab14b542
Rcpdroid <amos.marbun86@gmail.com>
2026-03-05 17:00:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -89,6 +89,11 @@ min-width:120px;
 th:nth-child(2), td:nth-child(2){{
 min-width:80px;
 }}
+
+section[data-testid="stSidebar"] {{
+padding-top:80px;
+}}
+
 section[data-testid="stSidebar"] {{
 margin-top:70px;
 }}
